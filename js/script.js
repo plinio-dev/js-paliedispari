a1242017228dca53371ae8e74468b9a4ecd017f9
Tommaso P <tommaso.plinio@gmail.com>
2021-03-08 15:20:47 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// 1 chiedo all'utente una parola
+// 2 stampo se è palindroma
+// 3 stampo se non è palindroma
